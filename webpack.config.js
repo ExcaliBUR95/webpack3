e91f2c8881b5862dc5f92d914be17dce49ce70ab
Mac <nahila_95@mail.ru>
2022-03-11 16:58:20 +0300

    done

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin') //старый импорт  к плагину
+const path = require('path') // 
+const {CleanWebpackPlugin} = require('clean-webpack-plugin') 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './indx.js',
-    mode: 'development',
-    module: {
-        rules: [
+    context: path.resolve(__dirname, 'src'),  //точка входа
+    entry: './indx.js', //точка входа
+    mode: 'development', //оптимизирует скорость приложения и ничего больше.
+    module: { //модули непонятно что это//Эти параметры определяют, как будут обрабатываться различные типы модулей в рамках проекта.
+        rules: [ //Правило можно разделить на три части - условия, результаты и вложенные правила.
             {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"]
+            test: /\.css$/i,  //не знаю что это, вроде настройка обработки чего то
+            use: ["style-loader", "css-loader"] // читается справа налево, знаю что это лоадеры, вроде подключают css, больше понятия не имею что это
         },
         {
             test: /\.(png|svg|jpg|gif)$/i,
@@ -17,34 +17,35 @@ module.exports = {
         },
         {
             test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
+            exclude: /node_modules/, // не нашел ничего такого об этом
+            use: { //юзаем бэйбл лудер
                 loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env']
+                options: { // опции
+                    presets: ['@babel/preset-env'] // непонятно
                 }
             } 
         }
         ]
     },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: './app.js'
+    output: { //точка выхода
+        path: path.resolve(__dirname, './dist'), // называем имя папки и указываем путь
+        filename: './app.js' //имя файла
     },
-    plugins: [
+    plugins: [ //плагины
         new HtmlWebpackPlugin({
-            path: path.resolve(__dirname, 'src'),
-            template: './index.html'
+            path: path.resolve(__dirname, 'src'), // путь к папке
+            template: './index.html' // путь к файлу, не знаю почему временной называется
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin() //сборщик мусора
     ],
-    resolve: {
-        extensions: ['.js', '.json', '.wasm']
+    resolve: { //не знаю что делает просто резерв
+        extensions: ['.js', '.json', '.wasm'] //Если несколько файлов имеют одинаковое имя,но разные расширения,webpack разрешит тот файл,расширение которого указано первым в массиве,и пропустит остальные.
     },
-    devServer: {
-        port: 3000
+    devServer: { // непонятно
+        port: 3000 // создаем порт
     },
 
 
 
 }
+//в пейкедж джейсоне вообще ничего не понимаю
